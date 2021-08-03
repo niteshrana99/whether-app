65ed30450265d5bf8c26b40f1fbac90ea9e65086
Nitesh Rana <rananitesh99@gmail.com>
2021-08-03 19:33:36 +0530

    Update errorfallback styling

--- src/ui/ErrorBoundary/ErrorFallBack.js
+++ src/ui/ErrorBoundary/ErrorFallBack.js
@@ -1,10 +1,10 @@
 
-const ErrorFallBack = ({error, resetErrorBoundary}) => {
+const ErrorFallBack = () => {
     return (
-        <div role="alert">
+        <div role="alert" style={{fontSize:"3rem"}}>
       There was an error:{' '}
-      <pre style={{whiteSpace: 'normal'}}>{error.message}</pre>
-      <button onClick={resetErrorBoundary}>Try again</button>
+      <pre style={{whiteSpace: 'normal'}}>Something went wrong. The developer has been notified</pre>
+      <h6>Please Refresh your application and Try Again</h6>
     </div>
     )
 }
